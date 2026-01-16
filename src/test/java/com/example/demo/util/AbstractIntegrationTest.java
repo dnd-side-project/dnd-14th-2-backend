3d50dc71365fafdff89c6bbebe74e7f262e8c0f9
GiSeok Kang <ime.rokz@gmail.com>
2026-01-16 20:25:29 +0900

    test: Testcontainers 제거

--- src/test/java/com/example/demo/util/AbstractIntegrationTest.java
+++ src/test/java/com/example/demo/util/AbstractIntegrationTest.java
@@ -3,21 +3,7 @@ package com.example.demo.util;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = NONE)
-@Transactional
-@Testcontainers
 public abstract class AbstractIntegrationTest {
-
-    @Container
-    @ServiceConnection
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-        .withDatabaseName("testdb")
-        .withUsername("test")
-        .withPassword("test");
 }
