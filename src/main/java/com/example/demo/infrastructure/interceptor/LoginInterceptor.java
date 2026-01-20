ced30066ff7ee40748448ff75aa9ae48b55b8dc5
lyouxsun <leedrkr323@konkuk.ac.kr>
2026-01-20 22:33:47 +0900

    fix: 스웨거 명세 생성 수정

--- src/main/java/com/example/demo/infrastructure/interceptor/LoginInterceptor.java
+++ src/main/java/com/example/demo/infrastructure/interceptor/LoginInterceptor.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 @Component
 @RequiredArgsConstructor
@@ -16,11 +17,17 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+
+        if (handler instanceof ResourceHttpRequestHandler) {
+            return true;
+        }
+
         String authorization = request.getHeader("Authorization");
 
         if (authorization == null || !authorization.startsWith("Bearer ")) {
-            // 로그인이 필요하다는 예외
-            throw new RuntimeException();
+            // 로그인이 필요 시 401로 응답
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
         }
 
         String token = authorization.substring("Bearer ".length());
