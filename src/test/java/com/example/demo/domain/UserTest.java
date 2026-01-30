9dc4cfd08a282d32c5456660bfa1c1d2ffca118b
GiSeok Kang <ime.rokz@gmail.com>
2026-01-30 22:09:20 +0900

    test: 유저 닉네임 등록 테스트 수정

--- src/test/java/com/example/demo/domain/UserTest.java
+++ src/test/java/com/example/demo/domain/UserTest.java
@@ -9,10 +9,10 @@ class UserTest {
     void 닉네임_첫_등록시_닉네임이_존재하면_예외가_발생한다() {
         // given
         User user = new User("test-email", "test-profile", Provider.GOOGLE, "test-provider-id");
-        user.registerNickname("test");
+        user.registerNickname(new Nickname("test"));
 
         // when & then
-        Assertions.assertThatThrownBy(() -> user.registerNickname("test2"))
+        Assertions.assertThatThrownBy(() -> user.registerNickname(new Nickname("test2")))
             .isInstanceOf(IllegalStateException.class)
             .hasMessage("이미 닉네임이 등록된 사용자입니다.");
     }
