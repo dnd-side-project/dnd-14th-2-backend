4924861b243ca2044217441ecedb97448873620a
GiSeok Kang <ime.rokz@gmail.com>
2026-01-31 21:44:39 +0900

    refactor: 탈퇴 시 DeleteMapping으로 변경

--- src/main/java/com/example/demo/infrastructure/controller/UserController.java
+++ src/main/java/com/example/demo/infrastructure/controller/UserController.java
@@ -4,7 +4,7 @@ import com.example.demo.application.UserService;
 import com.example.demo.infrastructure.interceptor.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +13,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/users/me/withdraw")
+    @DeleteMapping("/users/me")
     public ResponseEntity<Void> withdraw(@UserId Long userId) {
         userService.withdrawUser(userId);
         return ResponseEntity.noContent().build();
