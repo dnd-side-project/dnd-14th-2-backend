12f60e9cbc2a97bd1d094edcbf8bbcad7e53fee1
GiSeok Kang <ime.rokz@gmail.com>
2026-02-01 02:32:13 +0900

    test: 통합테스트 테이블 데이터 지우도록 기능 구현

--- src/test/java/com/example/demo/util/AbstractIntegrationTest.java
+++ src/test/java/com/example/demo/util/AbstractIntegrationTest.java
@@ -2,8 +2,35 @@ package com.example.demo.util;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
 
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest(webEnvironment = NONE)
 public abstract class AbstractIntegrationTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void before() {
+        // 외래키 체크 비활성화
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
+
+        // 모든 테이블 truncate
+        List<String> tables = jdbcTemplate.queryForList("""
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = DATABASE()
+        """, String.class);
+
+        for (String table : tables) {
+            jdbcTemplate.execute("TRUNCATE TABLE " + table);
+        }
+
+        // 외래키 체크 활성화
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
+    }
 }
