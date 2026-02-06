DND-14-2/DND-14-2-Backend

feat: 친구 추가 기능 구현

src/main/java/com/example/demo/domain/UserRepository.java
@@ -8,9 +8,11 @@ public interface UserRepository extends Repository<User, Long> {
 
     User save(User user);
 
+    Optional<User> findById(Long id);
+
     Optional<User> findByProviderAndProviderId(Provider provider, String providerId);
 
-    Optional<User> findById(Long id);
+    Optional<User> findByInvitationCode(String invitationCode);
 
     @Query("""
         select count(u) > 0

dnd-side-project/dnd-14th-2-backend

src/main/java/com/example/demo/domain/UserRepository.java
@@ -8,9 +8,11 @@ public interface UserRepository extends Repository<User, Long> {
 
     User save(User user);
 
+    Optional<User> findById(Long userId);
+
     Optional<User> findByProviderAndProviderId(Provider provider, String providerId);
 
-    Optional<User> findById(Long userId);
+    Optional<User> findByInvitationCode(String invitationCode);
 
     @Query("""
         select count(u) > 0