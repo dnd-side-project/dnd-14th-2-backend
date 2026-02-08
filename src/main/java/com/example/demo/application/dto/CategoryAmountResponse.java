250af82a63048c4661def6719e62a61562bd6bad
lyouxsun <leedrkr323@konkuk.ac.kr>
2026-02-08 21:56:06 +0900

    chore: 불필요한 클래스 삭제

--- src/main/java/com/example/demo/application/dto/CategoryAmountResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.application.dto;
-
-import com.example.demo.domain.enums.LedgerCategory;
-
-public record CategoryAmountResponse(
-    LedgerCategory category,
-    Long totalAmount
-) {
-}
