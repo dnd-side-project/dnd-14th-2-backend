244c901902dafb4aa13bac34ee0d9abf4a6d6d8e
GiSeok Kang <ime.rokz@gmail.com>
2026-02-12 23:23:17 +0900

    refactor: 사용하지 않는 테스트 변수 제거

--- src/test/java/com/example/demo/application/oauth/OauthAuthenticatorTest.java
+++ src/test/java/com/example/demo/application/oauth/OauthAuthenticatorTest.java
@@ -29,7 +29,6 @@ public class OauthAuthenticatorTest extends AbstractIntegrationTest {
         String idToken = "test-id-token";
         String providerId = "google-123456";
         String email = "test@example.com";
-        String picture = "https://example.com/picture.jpg";
 
         OauthUserInfo oauthUserInfo = new OauthUserInfo(providerId, email);
         given(idTokenVerifier.verifyAndGetUserInfo(provider, idToken)).willReturn(oauthUserInfo);
