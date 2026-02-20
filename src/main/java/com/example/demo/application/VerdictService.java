4d7181408f20f8d16a7f3d2e7975d35bdaf6a123
GiSeok Kang <ime.rokz@gmail.com>
2026-02-20 18:34:50 +0900

    feat: 판결 요청 기능 구현
    - 친구와 연동 필요하여 이 부분 제외

--- src/main/java/com/example/demo/application/VerdictService.java
+++ src/main/java/com/example/demo/application/VerdictService.java
@@ -1,5 +1,7 @@
 package com.example.demo.application;
 
+import com.example.demo.domain.LedgerEntry;
+import com.example.demo.domain.LedgerEntryRepository;
 import com.example.demo.domain.User;
 import com.example.demo.domain.UserRepository;
 import com.example.demo.domain.Verdict;
@@ -7,14 +9,30 @@ import com.example.demo.domain.VerdictRepository;
 import com.example.demo.domain.VerdictType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class VerdictService {
 
+    private final LedgerEntryRepository ledgerEntryRepository;
     private final UserRepository userRepository;
     private final VerdictRepository verdictRepository;
 
+    public void requestVerdict(Long ledgerId, Long userId) {
+        LedgerEntry ledgerEntry = ledgerEntryRepository.findByIdAndUser_Id(ledgerId, userId)
+            .orElseThrow();
+
+
+        // 모든 친구에게 요청
+        /*
+        friendRepository.findAll().stream()
+            .map(juror -> ledgerEntry.requestVerdict(juror))
+            .forEach(verdict -> verdictRepository.save(verdict)); or .toList(); verdictRepository.saveAll(verdicts);
+        */
+    }
+
+    @Transactional
     public void judge(Long verdictId, Long jurorId, VerdictType verdictType) {
         Verdict verdict = verdictRepository.findById(verdictId)
             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 판결입니다."));
--- src/main/java/com/example/demo/infrastructure/controller/VerdictController.java
+++ src/main/java/com/example/demo/infrastructure/controller/VerdictController.java
@@ -1,6 +1,7 @@
 package com.example.demo.infrastructure.controller;
 
 import com.example.demo.application.VerdictService;
+import com.example.demo.infrastructure.controller.dto.RequestJudgeWebRequest;
 import com.example.demo.infrastructure.controller.dto.VerdictJudgeWebRequest;
 import com.example.demo.infrastructure.interceptor.UserId;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class VerdictController {
 
     private final VerdictService verdictService;
+
+    public ResponseEntity<Void> requestJudge(RequestJudgeWebRequest request, @UserId Long userId) {
+        verdictService.requestVerdict(request.ledgerEntryId(), userId);
+        return ResponseEntity.ok().build();
+    }
 
     public ResponseEntity<Void> judge(VerdictJudgeWebRequest request, @UserId Long jurorId) {
         verdictService.judge(request.verdictId(), jurorId, request.verdictType());
