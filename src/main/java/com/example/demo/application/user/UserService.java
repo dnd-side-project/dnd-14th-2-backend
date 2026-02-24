a8a38182b59afe1cfe02738a8bf1939f5f601e08
lyouxsun <leedrkr323@konkuk.ac.kr>
2026-02-25 07:11:10 +0900

    refactor: UserInfo 팩토리 메서드 적용

--- src/main/java/com/example/demo/application/user/UserService.java
+++ src/main/java/com/example/demo/application/user/UserService.java
@@ -29,7 +29,7 @@ public class UserService {
     @Transactional(readOnly = true)
     public UserInfo getUserInfo(Long userId) {
         User user = findUserById(userId);
-        return new UserInfo(user.getId(), user.getNickname(), user.getLevel(), user.getProfile(), user.getInvitationCode().value());
+        return UserInfo.from(user);
     }
 
     @Transactional
