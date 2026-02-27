1b3840d7d8b3119a2faee1708d4811ec5a6ed1d5
GiSeok Kang <ime.rokz@gmail.com>
2026-02-27 20:20:59 +0900

    refactor: 응답값에 배심원 정보 포함하도록 변경

--- src/main/java/com/example/demo/infrastructure/controller/dto/MyVerdictWebResponse.java
+++ src/main/java/com/example/demo/infrastructure/controller/dto/MyVerdictWebResponse.java
@@ -2,11 +2,13 @@ package com.example.demo.infrastructure.controller.dto;
 
 import com.example.demo.application.dto.LedgerEntryInfo;
 import com.example.demo.application.dto.MyVerdict;
+import com.example.demo.application.dto.UserInfo;
 import com.example.demo.domain.VerdictType;
 
 public record MyVerdictWebResponse(
     Long id,
     LedgerEntryInfoWebResponse ledgerEntryInfo,
+    UserInfo juror,
     VerdictType verdictType
 ) {
     public static MyVerdictWebResponse from(MyVerdict myVerdict) {
@@ -21,6 +23,7 @@ public record MyVerdictWebResponse(
                 ledgerEntryInfo.paymentMethod(),
                 ledgerEntryInfo.description()
             ),
+            myVerdict.juror(),
             myVerdict.verdictType()
         );
     }
