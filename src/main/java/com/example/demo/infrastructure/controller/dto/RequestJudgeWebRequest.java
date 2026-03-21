5210aa66174ebaaffa234a87dfeda2dbed9f0fe0
GiSeok Kang <ime.rokz@gmail.com>
2026-03-21 18:04:52 +0900

    refactor: n+1 문제 해결

--- src/main/java/com/example/demo/infrastructure/controller/dto/RequestJudgeWebRequest.java
+++ src/main/java/com/example/demo/infrastructure/controller/dto/RequestJudgeWebRequest.java
@@ -1,9 +1,9 @@
 package com.example.demo.infrastructure.controller.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record RequestJudgeWebRequest(
-    @NotBlank
+    @NotNull
     Long ledgerEntryId
 ) {
 }
